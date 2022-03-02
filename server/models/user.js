0b3799d9efd2a87f3057be5ddacf4f948a504c65
Abhijeet Sinha <abhijeetaman007@gmail.com>
2022-03-02 18:17:54 +0530

    changes in routes,models

--- server/models/user.js
+++ server/models/user.js
@@ -1,72 +1,125 @@
-// Same User schema with requireds fields
-const mongoose = require('mongoose');
-const UserSchema = new mongoose.Schema({
+const mongoose = require("mongoose");
+const file = require("./file");
+
+const UserSchema = new mongoose.Schema(
+  {
     //userID: will be used as ID in frontend for each user not mongo's _id
     userID: {
-        type: Number,
-        required: true,
-        unique: true,
+      type: Number,
+      required: true,
+      unique: true,
     },
     role: {
-        type: String,
-        default: 'USER',
-        enum: ['USER'],
+      type: String,
+      default: "USER",
+      enum: ["USER"],
     },
-    timeStamp: {
-        type: Date,
+    token: {
+      type: String,
+      default: "",
+    },
+    passwordResetToken: {
+      type: String,
+      default: "",
     },
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     isEmailVerified: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
     mobileNumber: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
     },
     registrationNumber: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     branch: {
-        type: String,
-        default: '',
+      type: String,
+      default: "",
     },
     college: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     state: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+    },
+    isMIT:{
+      type:Boolean,
+      required:true,
     },
     isMahe: {
-        type: Boolean,
-        required: true,
+      type: Boolean,
+      required: true,
     },
-    accommodationRequired: {
+    accommodation: {
+      required: {
         type: Boolean,
         default: false,
+      },
+      arrivalDate:{
+        type:String
+      },
+      arrivalTime:{
+        type:String
+      },
     },
-    accommodationType: {
-        type: String,
-        enum: ['TIER-1', 'TIER-2'],
-    },
-    driveLink: {
-        type: String,
+    documents: {
+      aadhar: {
+        type: file.FileSchema,
+      },
+      vaccination: {
+        type: file.FileSchema,
+      },
+      undertaking:{
+        type:file.FileSchema
+      },
     },
     verified: {
-        type: String,
-        enum: ['VERIFIED', 'REJECTED', 'UNVERIFIED'],
-        default: 'UNVERIFIED',
+      type: String,
+      enum: ["VERIFIED", "REJECTED", "UNVERIFIED"],
+      default: "UNVERIFIED",
     },
-});
+    regEvents: [
+      {
+        event: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Event",
+        },
+        // Will be part of judges portal and result model
+        // isPresent: {
+        //   type: Boolean,
+        //   default: false,
+        // },
+      },
+    ],
+    //Stores all types of delegate Card/Tickets
+    // NOTE: Each user should have max one delegate card of each type
+    delegateCard: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "DelCard",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-module.exports = User = mongoose.model('User', UserSchema);
+module.exports = User = mongoose.model("User", UserSchema);
